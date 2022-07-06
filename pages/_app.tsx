57d551d6494690db7b2242de10b88475b453a810
tw1ttt3r <tw1ttt3r@gmail.com>
2022-07-05 19:43:21 -0500

    fix: replaced import to tailwind css file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '../styles/app.css'
+import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
