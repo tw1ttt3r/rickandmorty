9ce636f43144db95781fb7293ccbd413fdeb2ce7
tw1ttt3r <tw1ttt3r@gmail.com>
2022-07-11 18:35:48 -0500

    fix: cleaned home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
-import RandomCharacter from "@/components/RandomCharacter"
-
 export default function Home() {
   return (
-    <>
-      <header className="w-full">
-        <nav className="w-full">
-          <h1 className="text-white text-2xl flex justify-center">Rick y Morty</h1>
-        </nav>
-      </header>
-      <section className="h-full flex flex-col justify-center items-center">
-        <RandomCharacter />
-      </section>
-    </>
+    <h1>Hola mundo</h1>
   )
 }
